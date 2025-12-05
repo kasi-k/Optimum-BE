98a2ef7cfbe13d455b884cea70253629aac7293b
kasi-k <kasivishwanath698@gmail.com>
2025-12-05 12:31:51 +0530

    location

--- Optimum/src/config/VerifyLocation.js
+++ Optimum/src/config/VerifyLocation.js
@@ -1,23 +1,20 @@
-
-
-// utils/checkLocation.js
 export const isWithinOffice = (userLat, userLng, officeLat, officeLng, radius = 0.5) => {
   const toRad = (value) => (value * Math.PI) / 180;
 
   const R = 6371; // Earth radius in km
-  const dLat = toRad(officeLat - userLat);
-  const dLng = toRad(officeLng - userLng);
+
+  const dLat = toRad(userLat - officeLat);
+  const dLng = toRad(userLng - officeLng);
 
   const a =
     Math.sin(dLat / 2) ** 2 +
-    Math.cos(toRad(userLat)) *
     Math.cos(toRad(officeLat)) *
-    Math.sin(dLng / 2) ** 2;
+      Math.cos(toRad(userLat)) *
+      Math.sin(dLng / 2) ** 2;
 
-  const distance = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
+  const distance = R * c; // convert to KM
 
-  return distance <= radius; // true if within radius
+  return distance <= radius;
 };
-
-
